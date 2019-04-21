7b93b158a69a3c997c29274d9cd321038389c715
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-21 21:10:36 +0200

    Edit User Migration [NULLABEL]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('address');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->string('national_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
