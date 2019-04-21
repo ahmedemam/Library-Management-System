07d7513a2db9e1d92c72865eb2bfb710218cc0b6
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-21 21:02:13 +0200

    Add PostgresQL to Heroku

--- config/database.php
+++ config/database.php
@@ -1,6 +1,13 @@
 <?php
-use Illuminate\Support\Str;
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
+use Illuminate\Support\Str;
 return [
     /*
     |--------------------------------------------------------------------------
@@ -12,7 +19,7 @@ return [
     | you may use many connections at once using the Database library.
     |
      */
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -54,12 +61,11 @@ return [
             ]) : [],
         ],
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
