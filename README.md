af5893a2eec1467e8e3037c076299f46b5afd405
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-21 19:12:35 +0200

    fix API KEY

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 * Database: lib_mngmt_sys
 * Username: ahmedemam
 * Email: zizoahmed980@gmail.com
+base64:pIgjICL9EWkkk38ZcPdQlv+lSAA2/LsRfkD4NlrzJFE=
--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'lib_mngmt_sys'),
             'username' => env('DB_USERNAME', 'ahmedemam'),
-            'password' => env('DB_PASSWORD', '011421264482014zizo'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
