f6d6b46e8661fb859c688188055ea521bea0ee10
AhmedElshall <ahmed.elshall93@gmail.com>
2019-04-22 17:15:53 +0200

    adding realtions to Favourite Model

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
-}
+    public function books(){
+
+        return $this->hasMany('App\Book');
+    }
+
+     public function user(){
+
+            return $this->belongsTo('App\Book');
+        }
+    }
