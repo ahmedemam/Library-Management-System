ba54485fc81287ed030148090a11e0a87ea7b329
saratarek <saratarek224@hotmail.com>
2019-04-22 17:59:19 +0200

    seeders and factory for review model

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Review;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'rate' => $faker->numberBetween($min = 1, $max = 95),
+        'review' => $faker->text(),
+        'userId' => factory('App\User')->create()->id,
+        'bookId' => factory('App\Book')->create()->id,
+    ];
+});
---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        factory('App\Review', 500)->create();
+    }
+}
