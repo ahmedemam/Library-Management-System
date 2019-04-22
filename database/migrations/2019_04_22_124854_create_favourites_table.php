f91c1572ccf3e8ba07138c8b257381ed033418e7
AhmedElshall <ahmed.elshall93@gmail.com>
2019-04-22 15:57:01 +0200

    Favourite Model

---
+++ database/migrations/2019_04_22_124854_create_favourites_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavouritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favourites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('book_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+
+            $table->foreign('book_id')
+            ->references('id')
+            ->on('books');
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favourites');
+    }
+}
