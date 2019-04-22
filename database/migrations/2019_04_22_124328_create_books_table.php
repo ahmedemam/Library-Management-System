efe2208826fcd19cfe87efdb581020cdf58df227
mostafa <mostapha.a.aly@gmail.com>
2019-04-22 15:31:28 +0200

    Creating Book Model

---
+++ database/migrations/2019_04_22_124328_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->text('author');
+            $table->text('image');
+            $table->text('copiesNumber');
+            $table->text('leaseFee');
+            $table->foreign('categ_id')
+            ->reference('id')
+            ->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
