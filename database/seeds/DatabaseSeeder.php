d45eef132063e44d43549d21f000e9857f2a1005
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-24 12:34:40 +0200

    try to run seeders 2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+//        $this->call(CategoriesTableSeeder::class);
 //        $this->call(BooksTableSeeder::class);
 //        $this->call(ReviewsTableSeeder::class);
 //        $this->call(FavouriteTableSeeder::class);
