0e4eae0f770fa06a79043e13e0a8a3dd2cead0d3
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-26 19:40:53 +0200

    user/admin controller cont...

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('admin/', 'HomeController@admin')->middleware('admin');
