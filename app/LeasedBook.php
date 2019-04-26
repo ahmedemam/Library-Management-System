d9440674762047f39d185c6cc828f7797cdbd886
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-04-26 13:22:32 +0200

    update leased books

--- app/LeasedBook.php
+++ app/LeasedBook.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class LeasedBooks extends Model
+class LeasedBook extends Model
 {
     public function user(){
         return $this->belongsTo('App\User');
