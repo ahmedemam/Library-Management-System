09598d45b6e60f57b7b929f62ccb121cfdcf6a2c
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-05-06 16:24:57 +0200

    Login based Status

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -41,6 +42,15 @@ class LoginController extends Controller
         $this->username = $this->findUsername();
     }
 
+    protected function credentials(Request $request)
+    {
+        return [
+            'email' => $request->{$this->username()},
+            'password' => $request->password,
+            'status' => 'yes',
+        ];
+    }
+
     /**
      * Get the login username to be used by the controller.
      *
@@ -61,5 +71,36 @@ class LoginController extends Controller
     public function username()
     {
         return $this->username;
+    }
+
+    public function login(Request $request) {
+        $this->validateLogin($request);
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        // This section is the only change
+        if ($this->guard()->validate($this->credentials($request))) {
+            $user = $this->guard()->getLastAttempted();
+            // Make sure the user is active
+            if ($user->status && $this->attemptLogin($request)) {
+                // Send the normal successful login response
+                return $this->sendLoginResponse($request);
+            } else {
+                // Increment the failed login attempts and redirect back to the
+                // login form with an error message.
+                $this->incrementLoginAttempts($request);
+                return redirect()
+                    ->back()
+                    ->withInput($request->only($this->username(), 'remember'))
+                    ->withErrors(['status' => 'You must be active to login.']);
+            }
+        }
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+        return $this->sendFailedLoginResponse($request);
     }
 }
