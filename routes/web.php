534bdcb10ebc01a2b335b903eb9ee4d54e1be3ab
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-05-06 19:26:31 +0200

    Fixing others errors when i get pulled last time [Thanks Guys] =D

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,25 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BookController;
+
 
 Route::get("admin/routes", "HomeController@admin")->middleware("admin");
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register']);;
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/favourite', 'FavouriteController');
 Auth::routes(['register' => false]);
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/', 'HomeController@index')->name('home');
 Route::resource('/favourites', 'FavouriteController');
-
 Route::resource('/admin', 'AdminController');
-Route::resource('/user', 'UserController');
-
+Route::resource('/user', 'AdminController');
 Route::get('logout', function () {
     Auth::logout();
     return Redirect::to('login');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('admin/', 'HomeController@admin')->middleware('admin');
@@ -42,20 +40,16 @@ Route::get('/category/create', 'CategoryController@create');
 Route::get('/category', 'CategoryController@index');
 Route::post('/category/update', 'CategoryController@update');
 Route::resource('/category','CategoryController');
-});
 Route::get('/category/create', 'CategoryController@create');
 Route::get('/category/getall/{id}', 'CategoryController@getallbooks');
 Route::get('/category', 'CategoryController@index');
-
 Route::post('/category/update', 'CategoryController@update');
 Route::resource('/category', 'CategoryController');
 //! books routes
 Route::post('books/search', 'BookController@search')->name('books.search');
-
 Route::get('books/latest', 'BookController@getLatest')->name('books.latest');
 Route::get('books/rate', 'BookController@getHighRated')->name('books.rate');
 Route::resource('/books', 'BookController');
 Route::get('/books/lease/{id}/{user_id}', 'CategoryController@leasebooks');
-
 Route::resource('/comments', 'CommentController');
 Route::resource('books.comments', 'CommentController');
