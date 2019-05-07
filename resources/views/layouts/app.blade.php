eac3978f0a3a5fe01c57d309f584a2d33d2bfdf1
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-05-07 11:02:02 +0200

    user profile 5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
 					<!-- Left Side Of Navbar -->
 					<ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('AdminController@userProfile')}}">Profile</a>
+                            <a class="nav-link" href="{{url('/user/profile')}}">Profile</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/admin">Users</a>
