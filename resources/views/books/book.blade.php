8b4e69c7a61c7b3edb3db1f2c284998de72c7ce4
mostafa <mostapha.a.aly@gmail.com>
2019-05-07 15:44:04 +0200

    styling some pages

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -1,96 +1,94 @@
 @extends('layouts.app')
 @section('content')
 
-    <main class="container">
-        {{-- book data --}}
-        <section class="row">
-            <div class="col-sm-8">
-                <div class="media">
-                    {{-- /storage/book_images/ --}}
-                    <img src="{{$book->image}}" class="mr-3" alt="{{$book->title}}" width="200"
-                         height="350">
-                    <div class="media-body">
-                        <h5 class="mt-0">{{$book->title}}</h5>
-                        <p>
-                            {{$book->description}}
-                        </p>
-                        <p class="m-0">{{number_format($book->copiesNumber)}} copies available</p>
-                        <div>
-                            <button class="btn btn-success mt-3">Lease</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <form action="{{route('favourites.store')}}" method="POST">
-                    {{ csrf_field() }}
-                    <input type="hidden" value={{Auth::id()}} name="user_id">
+<main class="container">
+	{{-- book data --}}
+	<section class="row">
+		<div class="col-sm-8">
+			<div class="media">
+				{{-- /storage/book_images/ --}}
+				<img src="{{$book->image}}" class="mr-3" alt="{{$book->title}}" width="200" height="350">
+				<div class="media-body">
+					<h5 class="mt-0">{{$book->title}}</h5>
+					<p>
+						{{$book->description}}
+					</p>
+					<p class="m-0">{{number_format($book->copiesNumber)}} copies available</p>
+					<div>
+						<button class="btn btn-success mt-3">Lease</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<form action="{{route('favourites.store')}}" method="POST">
+				{{ csrf_field() }}
+				<input type="hidden" value={{Auth::id()}} name="user_id">
 
-                    <input type="hidden" value={{$book->id}} name="book_id">
-                    <button type="submit" class="btn btn-danger">Fav</button>
-                </form>
-            </div>
-        </section>
+				<input type="hidden" value={{$book->id}} name="book_id">
+				<button type="submit" class="btn btn-danger">Fav</button>
+			</form>
+		</div>
+	</section>
 
-        {{-- comments --}}
-        <section>
-
-
-            <div class="row" style='margin-top: 10px; margin-bottom: 10px;'>
-                <form action="{{ route('comments.store')}}" method='POST'>
-                    <form action="{{ route('comments.store')}}" method='POST'>
-                        {{ csrf_field() }}
-                        <input type="text" name='review' class='form-control'>
-                        <input type="number" name='rate' class='form-control'>
-                        <input type="hidden" name='book_id' value="{{$book->id}}">
-                        <input type="submit" class='btn btn-danger' value='Add Comment'>
-                    </form>
-            </div>
+	{{-- comments --}}
+	<section>
 
 
-            {{-- display stored book comments --}}
-            @if (count($storedComments ) > 0)
-                <table class="table">
-                    <thead>
+		<div class="row" style='margin-top: 10px; margin-bottom: 10px;'>
+			<form action="{{ route('comments.store')}}" method='POST'>
+				<form action="{{ route('comments.store')}}" method='POST'>
+					{{ csrf_field() }}
+					<input type="text" name='review' class='form-control' placeholder="comment">
+					<input type="number" min="1" max="5" name='rate' class='form-control' placeholder="rating">
+					<input type="hidden" name='book_id' value="{{$book->id}}">
+					<input type="submit" class='btn btn-danger' value='Add Comment'>
+				</form>
+		</div>
 
-                    <th>User</th>
-                    <th>comment</th>
-                    <th>Rate</th>
-                    <th>Delete</th>
-                    </thead>
 
-                    <tbody>
-                    @foreach ($storedComments as $storedComment)
-                        <tr>
-                            <th>{{ $storedComment->user->name }}</th>
-                            <td>{{ $storedComment->review }}</td>
-                            <td>{{ $storedComment->rate }}</td>
-                            <td>
-                                @can('delete', $storedComment)
-                                    <form action="{{ route('comments.destroy', ['reviews'=>$storedComment->id]) }}"
-                                    <form action="{{ route('comments.destroy', ['reviews'=>$storedComment->id]) }}"
-                                          method='POST'>
-                                        {{ csrf_field() }}
+		{{-- display stored book comments --}}
+		@if (count($storedComments ) > 0)
+		<table class="table">
+			<thead>
 
-                                        <input type="hidden" name='_method' value='DELETE'>
+				<th>User</th>
+				<th>comment</th>
+				<th>Rate</th>
+				<th>Delete</th>
+			</thead>
 
-                                        <input type="submit" class='btn btn-danger' value='Delete'>
-                                    </form>
+			<tbody>
+				@foreach ($storedComments as $storedComment)
+				<tr>
+					<th>{{ $storedComment->user->name }}</th>
+					<td>{{ $storedComment->review }}</td>
+					<td>{{ $storedComment->rate }}</td>
+					<td>
+						@can('delete', $storedComment)
+						<form action="{{ route('comments.destroy', ['reviews'=>$storedComment->id]) }}" <form
+							action="{{ route('comments.destroy', ['reviews'=>$storedComment->id]) }}" method='POST'>
+							{{ csrf_field() }}
 
-                                @endcan
+							<input type="hidden" name='_method' value='DELETE'>
 
-                            </td>
+							<input type="submit" class='btn btn-danger' value='Delete'>
+						</form>
 
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            @endif
-        </section>
-        {{-- related Books --}}
-        <section>
+						@endcan
 
-        </section>
+					</td>
 
-    </main>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		@endif
+	</section>
+	{{-- related Books --}}
+	<section>
+
+	</section>
+
+</main>
 @endsection
